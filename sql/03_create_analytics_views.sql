948fae9f24bce16f39f52058e89d10a21e9c4ae2
maru1406 <marushikakalyan27@gmail.com>
2026-09-25 22:52:48 +0530

    Add analytics views SQL

---
+++ sql/03_create_analytics_views.sql
@@ -0,0 +1,59 @@
+-- 1. Monthly sales performance
+
+CREATE OR REPLACE VIEW
+  `e-commerce-elt-pipeline-509716.ecommerce.v_monthly_sales` AS
+
+SELECT
+  DATE_TRUNC(order_date, MONTH) AS month,
+  COUNT(DISTINCT order_id) AS total_orders,
+  SUM(quantity) AS units_sold,
+  ROUND(SUM(total_sales), 2) AS revenue,
+  ROUND(AVG(total_sales), 2) AS average_order_value
+FROM `e-commerce-elt-pipeline-509716.ecommerce.clean_sales`
+GROUP BY month;
+
+
+-- 2. Category performance
+
+CREATE OR REPLACE VIEW
+  `e-commerce-elt-pipeline-509716.ecommerce.v_category_sales` AS
+
+SELECT
+  category,
+  COUNT(DISTINCT order_id) AS total_orders,
+  SUM(quantity) AS units_sold,
+  ROUND(SUM(total_sales), 2) AS revenue,
+  ROUND(AVG(total_sales), 2) AS average_order_value
+FROM `e-commerce-elt-pipeline-509716.ecommerce.clean_sales`
+GROUP BY category;
+
+
+-- 3. Regional performance
+
+CREATE OR REPLACE VIEW
+  `e-commerce-elt-pipeline-509716.ecommerce.v_region_sales` AS
+
+SELECT
+  region,
+  COUNT(DISTINCT order_id) AS total_orders,
+  SUM(quantity) AS units_sold,
+  ROUND(SUM(total_sales), 2) AS revenue,
+  ROUND(AVG(total_sales), 2) AS average_order_value
+FROM `e-commerce-elt-pipeline-509716.ecommerce.clean_sales`
+GROUP BY region;
+
+
+-- 4. Product performance
+
+CREATE OR REPLACE VIEW
+  `e-commerce-elt-pipeline-509716.ecommerce.v_product_sales` AS
+
+SELECT
+  product,
+  category,
+  COUNT(DISTINCT order_id) AS total_orders,
+  SUM(quantity) AS units_sold,
+  ROUND(SUM(total_sales), 2) AS revenue,
+  ROUND(AVG(total_sales), 2) AS average_order_value
+FROM `e-commerce-elt-pipeline-509716.ecommerce.clean_sales`
+GROUP BY product, category;
